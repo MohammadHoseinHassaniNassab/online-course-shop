6fc8939fe03e36de79528836f8596f104a675297
MohammadHoseinHassaniNassab <mohammad.hassan1385.12@gmail.com>
2025-12-31 08:59:26 +0330

    add database information

--- components/connection.php
+++ components/connection.php
@@ -1,3 +1,3 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "", "lebaseton");
+$connection = mysqli_connect("sql12.freesqldatabase.com", "sql12813210", "k1mJh7SV8q", "sql12813210");
 ?>
